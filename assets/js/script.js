862038feb59197e940c1f2474e7a6f3d3fc68146
Mykhailo <sharkon90@gmail.com>
2023-11-28 19:50:26 -0500

    adding event listenet for second answer buttons in second screen, changing function for transforming starter screen to qestion screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@
 
 let gameTimeEL = document.querySelector("#game-time");
 
-let conteinerEL = document.querySelector("#conteiner");
+let containerEL = document.querySelector("#container");
 
 let secondsLeft = 10;
 
-let questinObject = {
+let questinObjectOne = {
     qText: "Some question here",
     answer: [
-        {text: "Answer1", correct: true  },
-        {text: "Answer2", correct: false}, 
-        {text: "Answer3", correct: false}, 
-        {text: "Answer4", correct: false}
-    ]
-    
+         "1. Answer1", 
+         "2. Answer2", 
+         "3. Answer3", 
+         "4. Answer4" 
+    ],
+    correctAnswer: "2"
 } 
 // init() function to add starter game interface
 function init(){
@@ -22,20 +22,16 @@ function init(){
 
     let h3 = document.createElement("h3");
     h3.textContent = `Coding Quiz Challenge`;
-    conteinerEL.appendChild(h3);
+    containerEL.appendChild(h3);
 
     let p1 = document.createElement("p");
-    p1.textContent = `Try to answer the following code-related questions whithin the time limit.`;
-    conteinerEL.appendChild(p1);
-
-    let p2 = document.createElement("p");
-    p2.textContent = `Keep in mind that incorrect answers will penalize your score/time by ten seconds!`;
-    conteinerEL.appendChild(p2);
+    p1.textContent = `Try to answer the following code-related questions whithin the time limit.` + `\n` +  `Keep in mind that incorrect answers will penalize your score/time by ten seconds`;
+    containerEL.appendChild(p1);
 
     let newButton = document.createElement("button");
     newButton.textContent = `Start Quiz`;
     newButton.setAttribute("id", "start-button")
-    conteinerEL.appendChild(newButton);
+    containerEL.appendChild(newButton);
 }
 init();
 
@@ -44,7 +40,7 @@ let startButtonEL = document.querySelector("#start-button");
 
 // Timer that count down from starting point to 0. Amd show it to main screen
  function setTime() {
-    let secondsLeft = 10;
+    
     let timerIntercal = setInterval(function(){
         secondsLeft--;
         gameTimeEL.textContent = ` ${secondsLeft}`;
@@ -57,43 +53,67 @@ let startButtonEL = document.querySelector("#start-button");
 // Create new buttons
 // function to change screen with quiz question and buttons
 
-function quizQuestion() {
+//remove main screen
+function removeScreen() {
+    let pOld = document.querySelector("p");
+    let buttonOld = document.querySelector("#start-button");
+    containerEL.removeChild(pOld);
+    containerEL.removeChild(buttonOld);
+}
+
+// add new screen
+function quizQuestion(text) {
     let h3 = document.createElement("h3");
     let h3old = document.querySelector("h3");
-    h3.textContent = `Quizz Question`;
-    conteinerEL.replaceChild(h3, h3old);
+    h3.textContent = `${text}`;
+    containerEL.replaceChild(h3, h3old);
     };
 
-// function to change paragraphe 
-function quizAnswers(){
-    let b = document.createElement("button");
-    let p = document.querySelector("p");
-    b.textContent = `Answer`;
-    conteinerEL.replaceChild(b, p);
-}
-
-// function to change Start Quiz button 
-function quizStartButtonChange(){
-    let b = document.createElement("button");
-    let p = document.querySelector("#start-button");
-    b.textContent = `Answer`;
-    conteinerEL.replaceChild(b, p);
-}
-
-// function to add new button
-function quizAddButton() {
-    let newButton = document.createElement("button");
-    newButton.textContent = `Answer`;
-    conteinerEL.appendChild(newButton);
+// function to add question answer
+function quizAnswers(text) {
+    let button = document.createElement("button");
+    button.textContent = `${text}`;
+    containerEL.appendChild(button);
 }
 
 // Start Button event listener that start the game 
 // startButtonEL.addEventListener("click", setTime);
 startButtonEL.addEventListener("click", function(){  
     console.log(`click`);
-    quizQuestion();
-    quizAnswers();
-    quizAnswers();
-    quizStartButtonChange()
-    quizAddButton();
+    removeScreen();
+    addScreen();
+    checkAnswer()
 });
+
+function addScreen(){
+    containerEL.setAttribute("id", "question-container")
+    quizQuestion(questinObjectOne.qText);
+// for logic to add all question text
+    for(let i = 0; i <questinObjectOne.answer.length; i++) {
+    quizAnswers(questinObjectOne.answer[i]);
+    }
+    
+};
+
+
+
+//  Event listener for button's answers for quiz question
+function checkAnswer(){
+let buttonEL = document.querySelectorAll("button");
+for(let i =0; i < buttonEL.length; i++){
+buttonEL[i].addEventListener("click", function(event){
+    console.log(`click2`)
+    var element = event.target;
+    text = element.textContent.split("");
+    console.log(text);
+    if(text[0] == questinObjectOne.correctAnswer) {
+        secondsLeft = secondsLeft + 10;
+        gameTimeEL.textContent = ` ${secondsLeft}`;
+    } else {
+        secondsLeft = secondsLeft - 5;
+        gameTimeEL.textContent = ` ${secondsLeft}`;
+    }
+
+})
+}
+}
