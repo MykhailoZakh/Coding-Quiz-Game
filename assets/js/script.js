cd2d29a6442cacc0f4236af9b39c378b97e41fcc
Mykhailo <sharkon90@gmail.com>
2023-11-30 15:07:21 -0500

    adding coments to index.html, and arranging positions of functions in index.html, and removing unnecessary comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,58 +6,52 @@ let containerEL = document.querySelector("#container");
 
 let secondsLeft = 40;
 
+// Question Array with 4 question objects
 let questionArray = [
     ({
-        qText: "Some question here1",
+        qText: "How do you declare a variable in JavaScript?",
         answer: [
-            "1. Answer11", 
-            "2. Answer21", 
-            "3. Answer31", 
-            "4. Answer41" 
+            "1. var", 
+            "2. All answer are correct", 
+            "3. let", 
+            "4. const" 
         ],
         correctAnswer: "2"
     }),
     ({
-        qText: "Some question here2",
+        qText: "How do you select an HTML element in JavaScript?",
         answer: [
-             "1. Answer12", 
-             "2. Answer22", 
-             "3. Answer32", 
-             "4. Answer42" 
+             "1. getHTML()", 
+             "2. getElementByHTML()", 
+             "3. querySelector()", 
+             "4. selectHTML()" 
         ],
         correctAnswer: "3"
     }),
     ({
-        qText: "Some question here3",
+        qText: "How to declare object in JavaScript?",
         answer: [
-             "1. Answer13", 
-             "2. Answer23", 
-             "3. Answer33", 
-             "4. Answer43" 
+             "1. const object = {};", 
+             "2. let object {};", 
+             "3. const objext = ();", 
+             "4. let object();" 
         ],
         correctAnswer: "1"
     }),
     ({
-        qText: "Some question here4",
+        qText: "Which one is correct loop in JavaScript?",
         answer: [
-             "1. Answer14", 
-             "2. Answer24", 
-             "3. Answer34", 
-             "4. Answer44" 
+             "1. for{i= ; i < array.length; i++}();", 
+             "2. loop(i= ; i < array.length; i++){};", 
+             "3. loop{i= ; i < array.length; i++}();", 
+             "4. for(i= ; i < array.length; i++){};" 
         ],
         correctAnswer: "4"
     })
 ];
-// let questionObjectOne = 
 
-// let questionObjectTwo = 
-
-// let questionObjectThree = 
-
-// let questionObjectFour = 
 // init() function to add starter game interface
 function init(){
-    console.log(`work`)
 
     let h3 = document.createElement("h3");
     h3.textContent = `Coding Quiz Challenge`;
@@ -74,7 +68,7 @@ function init(){
 }
 init();
 
-
+// selecting new HTML button element
 let startButtonEL = document.querySelector("#start-button");
 
 // Timer that count down from starting point to 0. Amd show it to main screen
@@ -85,7 +79,7 @@ let startButtonEL = document.querySelector("#start-button");
         gameTimeEL.textContent = ` ${secondsLeft}`;
         
         if(secondsLeft === NaN){
-            // gameTimeEL.textContent = NaN;
+           
             clearInterval(timerInterval);
             gameTimeEL.classList.add("hidden");
         }
@@ -96,15 +90,13 @@ let startButtonEL = document.querySelector("#start-button");
             removeQuestion();
             addFinalScreen();
             h4.classList.add("hidden");
-            // h4.textContent = '';
-            // gameTimeEL.textContent = `0`
+            
         }
        
     }, 1000);
 }
 
-// Create new buttons
-// function to change screen with quiz question and buttons
+// functions to change screen with quiz question and buttons
 
 //remove main screen
 function removeScreen() {
@@ -114,7 +106,7 @@ function removeScreen() {
     containerEL.removeChild(buttonOld);
 }
 
-// add new screen
+// change text in h3 element 
 function quizQuestion(text) {
     let h3 = document.createElement("h3");
     let h3old = document.querySelector("h3");
@@ -130,6 +122,7 @@ function quizAnswers(text) {
     containerEL.appendChild(button);
 }
 
+// function to delete all question buttons 
 function changeButtonsAnswers(question) {
     let button = document.querySelectorAll("button");
     for(let i = 0; i <question.length; i++) {
@@ -137,7 +130,7 @@ function changeButtonsAnswers(question) {
         }
 }
 
-
+// function to add content to screen after removing it
 function addScreen(question){
     containerEL.setAttribute("id", "question-container")
     quizQuestion(question.qText);
@@ -152,14 +145,16 @@ function addScreen(question){
     
 };
 
+// function to change screen between question
 function changeScreen(question) {
     quizQuestion(question.qText);
     changeButtonsAnswers(question.answer);
     for(let i = 0; i <question.answer.length; i++) {
         quizAnswers(question.answer[i]);
-        }
+    }
 }
 
+// functions to add text content with correct or wrong to h4 element
 function winCheckText() {
     let h4 = document.createElement("h4");
     let h4old = document.querySelector("h4");
@@ -167,166 +162,15 @@ function winCheckText() {
     containerEL.replaceChild(h4, h4old);
     };
 
-    function loseCheckText() {
-        let h4 = document.createElement("h4");
-        let h4old = document.querySelector("h4");
-        h4.textContent = `Wrong ❌`;
-        containerEL.replaceChild(h4, h4old);
-        };
-//  Event listener for button's answers for quiz question
-function checkAnswer(question){
-// let buttonEL = document.querySelector(".container");
-//     buttonEL.addEventListener("click", function(event)
-    let buttonEL = document.querySelectorAll("button");
-    for(i = 0; i < buttonEL.length; i++){
-    buttonEL[i].addEventListener("click", function(event)
-    {
-    console.log(`click2`)
-    event.stopPropagation();
-    // if (event.target.matches(".button")){
-        console.log('click');
-    let element = event.target;
-    text = element.textContent.split("");
-    
-    console.log('1');
-    
-    if(text[0] === question.correctAnswer) {
-        
-        
-        secondsLeft = secondsLeft + 10;
-        gameTimeEL.textContent = ` ${secondsLeft}`;
-        changeScreen(questionArray[1]);
-        winCheckText();
-        question2(questionArray[1]);
-        
-        // clearInterval(timerIntercal);
-        
-    } else {
-        
-        secondsLeft = secondsLeft - 5;
-        gameTimeEL.textContent = ` ${secondsLeft}`;
-        changeScreen(questionArray[1]);
-        loseCheckText();
-        question2(questionArray[1]);
-        
-        // clearInterval(timerIntercal);
-        
-    }
+function loseCheckText() {
+    let h4 = document.createElement("h4");
+    let h4old = document.querySelector("h4");
+     h4.textContent = `Wrong ❌`;
+    containerEL.replaceChild(h4, h4old);
+};
 
-    })
-}}
-
-// let buttonEL = document.querySelectorAll("button");
-// for(i = 0; i < buttonEL.length; i++){
-//     buttonEL[i].addEventListener("click", function(event)
-// }
-
-function question2(question){
-    // let buttonEL = document.querySelector(".container");
-    // buttonEL.addEventListener("click", function(event)
-    let buttonEL = document.querySelectorAll("button");
-    for(i = 0; i < buttonEL.length; i++){
-    buttonEL[i].addEventListener("click", function(event)
-    {
-    console.log(`click2`)
-    // event.stopPropagation();
-    // if (event.target.matches(".button")){
-        console.log('click');
-        let element = event.target;
-        text = element.textContent.split("");
-    
-        console.log('2');
-        if(text[0] === question.correctAnswer) {
-            secondsLeft = secondsLeft + 10;
-            gameTimeEL.textContent = ` ${secondsLeft}`;
-            changeScreen(questionArray[2]);
-            winCheckText();
-            question3(questionArray[2]);
-        } else {
-            secondsLeft = secondsLeft - 5;
-            gameTimeEL.textContent = ` ${secondsLeft}`;
-            changeScreen(questionArray[2]);
-            loseCheckText();
-            question3(questionArray[2]);
-    }
-
-    })
-
-}}
-
-
-function question3(question){
-    // let buttonEL = document.querySelector(".container");
-    // buttonEL.addEventListener("click", function(event)
-    let buttonEL = document.querySelectorAll("button");
-    for(i = 0; i < buttonEL.length; i++){
-    buttonEL[i].addEventListener("click", function(event)
-    {
-    console.log(`click2`)
-    // event.stopPropagation();
-    // if (event.target.matches(".button")){
-        console.log('click');
-        let element = event.target;
-        text = element.textContent.split("");
-    
-        console.log('3');
-        if(text[0] === question.correctAnswer) {
-            secondsLeft = secondsLeft + 10;
-            gameTimeEL.textContent = ` ${secondsLeft}`;
-            changeScreen(questionArray[3]);
-            winCheckText();
-            question4(questionArray[3]);
-            return;
-        
-        } else {
-            secondsLeft = secondsLeft - 5;
-            gameTimeEL.textContent = ` ${secondsLeft}`;
-            changeScreen(questionArray[3]);
-            loseCheckText();
-            question4(questionArray[3]);
-            return;
-        
-        }
-
-    })
-}}
-
-function question4(question){
-    // let buttonEL = document.querySelector(".container");
-    // buttonEL.addEventListener("click", function(event)
-    let buttonEL = document.querySelectorAll("button");
-    for(i = 0; i < buttonEL.length; i++){
-    buttonEL[i].addEventListener("click", function(event)
-    {
-    console.log(`click2`)
-    // event.stopPropagation();
-        // if (event.target.matches(".button")){
-            let h4 = document.querySelector("h4");
-            console.log('click');
-            let element = event.target;
-            text = element.textContent.split("");
-    
-        console.log('4');
-        if(text[0] == question.correctAnswer) {
-            secondsLeft = secondsLeft + 10;
-            gameTimeEL.textContent = ` ${secondsLeft}`;
-            removeQuestion();
-            addFinalScreen();
-            h4.classList.add("hidden");
-            
-        
-        } else {
-            secondsLeft = secondsLeft - 5;
-            gameTimeEL.textContent = ` ${secondsLeft}`;
-            removeQuestion();
-            addFinalScreen();
-            h4.classList.add("hidden");
-            
-        }
-
-    })
-}}
-
+// functions for the end screen
+// function to delete question screen
 function removeQuestion(){
     let h3 = document.querySelector("h3");
     containerEL.removeChild(h3);
@@ -336,7 +180,9 @@ function removeQuestion(){
         }
 }
 
+// function to add final screen and add functionality to it
 function addFinalScreen(){
+    // creating array for local storage and retrieving information from it
     inputValue = [];
     scoreValue =[];
     let storedInput = JSON.parse(localStorage.getItem("Initials"));
@@ -345,11 +191,11 @@ function addFinalScreen(){
             inputValue = storedInput;
             scoreValue = storedScore;
         }
+    // geting score info and stoping the timer function
     let score = secondsLeft;
     secondsLeft = NaN;
-    
-    // gameTimeEL.textContent = "0"
     gameTimeEL.classList.add("hidden");
+    // adding final screen 
     let h3 = document.createElement("h3");
     let paragraph = document.createElement("p");
     let form = document.createElement("form");
@@ -362,21 +208,16 @@ function addFinalScreen(){
     let label = document.createElement("label");
     let input = document.createElement("input");
     let button = document.createElement("button");
-    // let a = document.createElement("a");
     label.textContent = `Enter your initials:`;
     button.textContent = `Submit`;
     button.setAttribute("onclick", "location.href='./score.html'")
     button.setAttribute("id", "submit-button");
     label.setAttribute("id", "form-label");
-    // a.setAttribute("href", "./score.html");
     input.setAttribute("name", "submited-initails");
     form.appendChild(label);
     form.appendChild(input);
     form.appendChild(button);
-
-    // a.appendChild(button);
-
-    // Event listener for submit button
+    // Event listener for submit button and adding information from form input and score to local storage
     button.addEventListener("click", function(event) {
         event.preventDefault();
         var inputText = input.value.trim();
@@ -390,10 +231,126 @@ function addFinalScreen(){
       });
 }
 
+//  Event listener for button's answers for first quiz question
+function checkAnswer(question){
+
+    let buttonEL = document.querySelectorAll("button");
+
+    for(i = 0; i < buttonEL.length; i++){
+        buttonEL[i].addEventListener("click", function(event) {
+            event.stopPropagation();
+            let element = event.target;
+            text = element.textContent.split("");
+
+            if(text[0] === question.correctAnswer) {
+                secondsLeft = secondsLeft + 10;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                changeScreen(questionArray[1]);
+                winCheckText();
+                question2(questionArray[1]);    
+            } else {
+        
+                secondsLeft = secondsLeft - 5;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                changeScreen(questionArray[1]);
+                loseCheckText();
+                question2(questionArray[1]);
+            }
+
+        })
+    }
+}
+
+// Event listener for button's answers for second quiz question
+function question2(question){
+    let buttonEL = document.querySelectorAll("button");
+
+    for(i = 0; i < buttonEL.length; i++){
+        buttonEL[i].addEventListener("click", function(event) {
+            let element = event.target;
+            text = element.textContent.split("");
+    
+            if(text[0] === question.correctAnswer) {
+                secondsLeft = secondsLeft + 10;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                changeScreen(questionArray[2]);
+                winCheckText();
+                question3(questionArray[2]);
+            } else {
+                secondsLeft = secondsLeft - 5;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                changeScreen(questionArray[2]);
+                loseCheckText();
+                question3(questionArray[2]);
+            }
+
+        })
+
+    }
+}
+
+// Event listener for button's answers for third quiz question
+function question3(question){
+    let buttonEL = document.querySelectorAll("button");
+
+    for(i = 0; i < buttonEL.length; i++){
+        buttonEL[i].addEventListener("click", function(event) {
+            let element = event.target;
+            text = element.textContent.split("");
+    
+            if(text[0] === question.correctAnswer) {
+                secondsLeft = secondsLeft + 10;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                changeScreen(questionArray[3]);
+                winCheckText();
+                question4(questionArray[3]);
+        
+            } else {
+                secondsLeft = secondsLeft - 5;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                changeScreen(questionArray[3]);
+                loseCheckText();
+                question4(questionArray[3]);
+            }
+
+        })
+    }
+}
+
+// Event listener for button's answers for last quiz question
+function question4(question){
+    let buttonEL = document.querySelectorAll("button");
+
+    for(i = 0; i < buttonEL.length; i++){
+        buttonEL[i].addEventListener("click", function(event) {
+
+            let h4 = document.querySelector("h4");
+            let element = event.target;
+            text = element.textContent.split("");
+
+            if(text[0] == question.correctAnswer) {
+                secondsLeft = secondsLeft + 10;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                removeQuestion();
+                addFinalScreen();
+                h4.classList.add("hidden");
+            
+        
+            } else {
+                secondsLeft = secondsLeft - 5;
+                // gameTimeEL.textContent = ` ${secondsLeft}`;
+                removeQuestion();
+                addFinalScreen();
+                h4.classList.add("hidden");
+            }
+
+        })
+    }
+}
 
 
-// Start Button event listener that start the game 
-// startButtonEL.addEventListener("click", setTime);
+
+// Start Button's event listener that start the game 
 startButtonEL.addEventListener("click", function(){
     setTime()
     removeScreen();
